03a9b4b7cce5537366c779dcfe2e6be873721435
Zabro <reinzabrocki@shaw.ca>
2024-03-25 21:01:47 -0600

    Data collector code done

---
+++ Current_and_Time_Collection_Code-1.py
@@ -0,0 +1,71 @@
+from Phidget22.Phidget import *
+from Phidget22.Devices.CurrentInput import *
+import time
+import csv
+
+#https://www.phidgets.com/?view=api&product_id=VCP1100_0&lang=Python
+
+
+CurrentSensor = CurrentInput()
+TimeReading = 10 #seconds
+ReadingInterval = 0.25
+CSVfile= "Generator A vs C.csv"
+
+
+while True:
+    try:
+        CurrentSensor.openWaitForAttachment(1000)
+    except PhidgetException:
+        print("Something went wrong with the connecting the phidgets to the computer")
+    finally:
+        print("Everythng is good")
+        break 
+
+
+print("If ready press any key ALSO CLOSE THE CSV FILE")
+input()
+print('\n')
+
+Startingtime = time.time()
+print("The starting unix time is: ", Startingtime)
+time.sleep(1)
+print("Second callibration: ", time.time() - Startingtime)
+Currentlist = []
+Secondlist = []
+print("The loop is starting")
+print('\n')
+
+# Reassign time just so everything is accurate
+Startingtime = time.time()
+CurrentTime = time.time()
+
+while CurrentTime - Startingtime < TimeReading:
+    Currentlist.append(CurrentSensor.getCurrent())
+    Secondlist.append(time.time() - Startingtime)
+    CurrentTime = time.time()
+    time.sleep(ReadingInterval)
+    
+
+
+CurrentSensor.close()
+print(f"The reading is done,the length of the time(s) list is {len(Secondlist)} and the length of the current(A) list is {len(Currentlist)}")
+print("writing the data to a csv file, top row is current and bottom row is time")
+
+
+
+with open(CSVfile, 'w') as CSV:
+    Writer = csv.writer(CSV)
+    #First row is the current in amps
+    Writer.writerow(Currentlist)
+    #Second row is the time in seconds
+    Writer.writerow(Secondlist) 
+    
+
+
+
+
+
+
+print("Everything is done")
+
+
